aa17b8c8f65e81a2bb4f107c575fc8018c9ab3f5
Nandeeshmern <nandeesh.ns1994@gmail.com>
2019-07-27 17:44:27 +0530

    Mongoose

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
 const session = require('express-session');
-const routes = require('./routes/index')
-const errorHandlers = require('./middelwares/error-handlers');
-const appMiddleware = require('./middelwares/app-middleware');
+const indexRoutes = require('./routes/index');
+const projectRoutes = require('./routes/projects');
+const adminRoutes = require('./routes/admin');
+const errorHandlers = require('./middlewares/error-handlers');
+const appMiddleware = require('./middlewares/app-middelware')
 
 const app = express();
 
@@ -26,7 +28,71 @@ app.use(express.urlencoded({extended:false}));
 
 app.use(appMiddleware.authenticated);
 app.use(appMiddleware.logger);
+app.use('/', indexRoutes);
 
+app.use('/projects', projectRoutes);
+
+app.use('/admin',appMiddleware.authenticate, adminRoutes);
+// app.get('/', routes.index);
+// app.get('/projects', routes.project);
+
+// app.get('/signin', routes.signin);
+// app.post('/signin', routes.doSignin);
+// app.get('/signout', routes.signout);
+
+// app.get('/admin', appMiddleware.authenticate, routes.admin); 
+// app.get('/admin/projects', appMiddleware.authenticate, routes.adminProjects);
+
+// app.get('/admin/projects/:alias', appMiddleware.authenticate, routes.adminProjectDetail);
+
+
+// app.get('/projects/:alias', routes.projectDetail);
+
+app.use(errorHandlers.notFound);
+
+
+app.use(errorHandlers.handleError);
+
+app.listen(3000, () => console.log('Server up n running on port 3000'))
+
+
+/*const express = require('express');
+const hbs = require('hbs');
+const session = require('express-session');
+const routes = require('./routes/index')
+const indexRoutes=require('./routes/index')
+const projectRouter = require('./routes/projects')
+const errorHandlers = require('./middelwares/error-handlers');
+const appMiddleware = require('./middelwares/app-middleware');
+
+const app = express();
+
+app.set('views', __dirname+'/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+'/views/partials')
+
+
+
+app.use(session({
+    secret:'myappsecret',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {maxAge: 60000}
+}))
+
+app.use(express.static(__dirname+'/static'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(appMiddleware.authenticated);
+app.use(appMiddleware.logger);
+
+
+app.use('/', indexRoutes);
+
+app.use('/projects', projectRouter);
+
+app.use('/admin', adminRoutes);
 app.get('/', routes.index);
 app.get('/projects', routes.project);
 
@@ -37,6 +103,8 @@ app.get('/signout', routes.signout);
 app.get('/admin', appMiddleware.authenticate, routes.admin);
 app.get('/admin/projects', appMiddleware.authenticate, routes.adminProjects)
 
+app.get('/admin/projects/create-new', appMiddleware.authenticate, routes.createProjects)
+app.post('/admin/projects/create-new', appMiddleware.authenticate, routes.doCreateProjects)
 
 app.get('/projects/:alias', routes.projectDetail);
 
@@ -45,4 +113,4 @@ app.use(errorHandlers.notFound);
 
 app.use(errorHandlers.handleError);
 
-app.listen(3000, () => console.log('Server up n running on port 3000'))
+app.listen(3000, () => console.log('Server up n running on port 3000'))*/
--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,65 @@
-const data = require('../my-data')
+const express = require('express');
 
-module.exports.index = function(req,res) {
-    console.log(req.session)
+const router = express.Router();
+
+const data = require('../my-data')
+const MongoClient = require('mongodb').MongoClient;
+
+const dbUrl = 'mongodb://localhost:27017';
+
+let db;
+
+MongoClient.connect(dbUrl, {useNewUrlParser: true}, function(err, client) {
+    if(err) {
+        console.log(err)
+    }else {
+        console.log('Successfuly connected to DB ');
+        db = client.db('batch-3');
+    }
+})
+
+router.get('/', function(req,res) {
     res.render('index', {
         title:'My Portfolio',
         hasNavHome : true
     })
-}
+});
 
-
-module.exports.project = function(req, res) {
-
-    res.render('projects', {
-        layout:'layout',
-        title:'Projects',
-        projects: data.myProjects,
-        hasNavProject : true
-    })
-}
-
-module.exports.projectDetail = function(req,res) {
-
-    let alias = req.params.alias;
-    let index = data.projectIndex[alias];
-    let project = data.myProjects[index];
-    res.render('project-detail', {
-        title : 'Project',
-        projectDetail: project
-    })   
-}
-
-
-module.exports.signin = (req,res) => {
+router.get('/signin' ,(req,res) => {
+   
     res.render('signin', {
         layout:'signin-layout',
         title:'SignIn'
     })
-}
+})
 
 let users = [
-    {name:'nandi',email:'test@test.com', password:'test'},
-    {name:'nandi',email:'nandi@test.com', password:'test'}
+    {name:'ashu',email:'test@test.com', password:'test'},
+    {name:'nandi',email:'nandi@nandi.com', password:'test'}
 ]
 
-module.exports.doSignin = (req,res, next) => {
+router.post('/signin',(req,res, next) => {
     let bodyData = req.body;
 
-    console.log(bodyData)
+
     let usr = users.filter(e => e.email === bodyData.email)[0];
 
     console.log(usr)
     if(usr.password === bodyData.password) {
-
         req.session.user = usr;
         req.session.isLoggedIn = true;
-
-        console.log(req.session)
         res.redirect('/admin')
     }else {
         next(new Error('Password is wrong'))
     }
-}
-
-module.exports.admin = (req,res) => {
-    res.render('admin/index', {
-        title: 'Admin',
-        layout:'admin-layout'
-    })
-}
+})
 
 
-module.exports.signout = (req,res) => {
+router.get('/signout', (req,res) => {
     req.session.isLoggedIn = false;
     req.session.user = {};
 
-    res.redirect('/'); 
-}
+    res.redirect('/');
+})
 
-module.exports.adminProjects = (req,res) => {
-    res.render('admin/projects', {
-        title: 'Project List',
-        layout:'admin-layout',
-        projects: data.myProjects
-    })
-}
+module.exports = router;
